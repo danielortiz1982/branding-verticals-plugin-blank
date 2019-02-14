bbcf4295e9902a53b5704d2b18c6557ca14bd0a5
danielortiz1982 <danielortiz1982@gmail.com>
2019-02-14 12:56:50 -0500

    update

--- templates/bv-template.php
+++ templates/bv-template.php
@@ -1,1 +1,13 @@
-<h1>Welcome Branding Verticals</h1>
+<?php $args = array('post_type' => 'bv_blank', 'post_status' => 'publish', 'posts_per_page' => -1, 'orderby' => 'title', 'order' => 'ASC'); $the_query = new WP_Query( $args ); ?>
+<section class="bv-row">
+<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+  <div class="bv-3">
+    <div class="profile-box">
+      <div class="profile-img"><a href="<?php the_permalink()?>"><img class="img" src="<?php the_post_thumbnail_url(); ?>"></a></div>
+      <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+      <p><?php the_content(); ?></p>
+    </div>
+  </div>
+  <?php endwhile; ?>
+  <?php wp_reset_postdata(); ?>
+</section>
